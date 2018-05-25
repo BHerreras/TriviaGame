6f6305aaf0a565d146acccc69438265c5f455870
Bri <bherreras23@gmail.com>
2018-05-25 00:50:29 -0700

    Updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,137 +1,197 @@
-var correctAnswer = 0
-var wrongAnswer = 0
+
+var panel = $("#quiz-area");
+var countStartNumber = 30;
 
 
-$(document).ready(function() {
-    $('#start').on("click", function() {
-        // Display and start countdown
-        startTimer();
-        // Hide start button
-        $('#start').hide();
-        // Display 8 questions with answer choices in 'questions' div
-        displayQuestions();
-        // Display 'done' button
-        $('#Submit').show();
-    })
+var questions = [
+    {
+        question: 'The ocean makes up how  much of the earths surface?',
+        answers: ['40%', '71%', '36%', '87%'],
+        correctAnswer: '71%'
+    },
+    {
+        question: 'Where is the deepest point?',
+        answers: ['Mariana Trench', 'Philippine Trench', 'Eurasian Basin', 'Tonga Trench'],
+        correctAnswer: 'Mariana Trench'
+    },
+    {
+        question: 'From how far away can the Great Barrier Reef be seen?',
+        answers: ['Mesosphere', 'Top of Mount Everest', 'The Moon', 'Stratosphere'],
+        correctAnswer: 'The Moon'
+    },
+    {
+        question: 'How far does the Gray Whale migrate every year(the longest of any mammel)?',
+        answers: ['15,000 miles', '2,000 miles', '7,000 miles', '10,000 miles'],
+        correctAnswer: '10,000 miles'
+    },
+    {
+        question: 'What is the largest animal on earth EVER?',
+        answers: ['Megalodon', 'Blue Whale', 'Tyrannosaurus-Rex', 'Giant Squid'],
+        correctAnswer: 'Blue Whale'
+    },
+    {
+        question: 'How much of the worlds volcanic activity occurs in the ocean?',
+        answers: ['20%', '40%', '70%', '90%'],
+        correctAnswer: '90%'
+    },
+    {
+        question: 'How much of the underwater world remains unexplored?',
+        answers: ['95%', '73%', '16%', '0%'],
+        correctAnswer: '95%'
+    },
+    {
+        question: 'Geographers divide the ocean into how many parts?',
+        answers: ['2', '3', '4', '5'],
+        correctAnswer: '4'
+    },
+    {
+        question: 'What microscopic organisms are respnosible for producing about 1/2 of the worlds oxygen?',
+        answers: ['phytoplankton', 'seagrasses', 'sea anemones', 'Acantharea'],
+        correctAnswer: 'seagrasses'
+    },
+    {
+        question: 'Which shark can thrive in both salt and fresh water? ?',
+        answers: ['Sand', 'Bull', 'Thresher', 'Smooth-Hound'],
+        correctAnswer: 'Bull'
+    }
+
+];
+
+var timer;
+
+var game = {
+
+    questions: questions,
+    currentQuestion: 0,
+    counter: countStartNumber,
+    correct: 0,
+    incorrect: 0,
+
+    countdown: function () {
+        game.counter--;
+        $("#counter-number").text(game.counter);
+        if (game.counter === 0) {
+            console.log("TIME UP");
+            game.timeUp();
+        }
+    },
+
+    loadQuestion: function () {
+
+        timer = setInterval(game.countdown, 1000);
+
+        panel.html("<h2>" + questions[this.currentQuestion].question + "</h2>");
+
+        for (var i = 0; i < questions[this.currentQuestion].answers.length; i++) {
+            panel.append("<button class='answer-button' id='button' data-name='" + questions[this.currentQuestion].answers[i]
+                + "'>" + questions[this.currentQuestion].answers[i] + "</button>");
+        }
+    },
+
+    nextQuestion: function () {
+        game.counter = countStartNumber;
+        $("#counter-number").text(game.counter);
+        game.currentQuestion++;
+        game.loadQuestion();
+    },
+
+    timeUp: function () {
+
+        clearInterval(timer);
+
+        $("#counter-number").html(game.counter);
+
+        panel.html("<h2>Out of Time!</h2>");
+        panel.append("<h3>The Correct Answer was: " + questions[this.currentQuestion].correctAnswer);
+        panel.append("<img src='" + questions[this.currentQuestion].image + "' />");
+
+        if (game.currentQuestion === questions.length - 1) {
+            setTimeout(game.results, 3 * 1000);
+        }
+        else {
+            setTimeout(game.nextQuestion, 3 * 1000);
+        }
+    },
+
+    results: function () {
+
+        clearInterval(timer);
+
+        panel.html("<h2>All done, heres how you did!</h2>");
+
+        $("#counter-number").text(game.counter);
+
+        panel.append("<h3>Correct Answers: " + game.correct + "</h3>");
+        panel.append("<h3>Incorrect Answers: " + game.incorrect + "</h3>");
+        panel.append("<h3>Unanswered: " + (questions.length - (game.incorrect + game.correct)) + "</h3>");
+        panel.append("<br><button id='start-over'>Start Over?</button>");
+    },
+
+    clicked: function (e) {
+        clearInterval(timer);
+        if ($(e.target).attr("data-name") === questions[this.currentQuestion].correctAnswer) {
+            this.answeredCorrectly();
+        }
+        else {
+            this.answeredIncorrectly();
+        }
+    },
+
+    answeredIncorrectly: function () {
+
+        game.incorrect++;
+
+        clearInterval(timer);
+
+        panel.html("<h2>Nope!</h2>");
+        panel.append("<h3>The Correct Answer was: " + questions[game.currentQuestion].correctAnswer + "</h3>");
+        //panel.append("<img src='" + questions[game.currentQuestion].image + "' />");
+
+        if (game.currentQuestion === questions.length - 1) {
+            setTimeout(game.results, 3 * 1000);
+        }
+        else {
+            setTimeout(game.nextQuestion, 3 * 1000);
+        }
+    },
+
+    answeredCorrectly: function () {
+
+        clearInterval(timer);
+
+        game.correct++;
+
+        panel.html("<h2>Correct!</h2>");
+        //panel.append("<img src='" + questions[game.currentQuestion].image + "' />");
+
+        if (game.currentQuestion === questions.length - 1) {
+            setTimeout(game.results, 3 * 1000);
+        }
+        else {
+            setTimeout(game.nextQuestion, 3 * 1000);
+        }
+    },
+
+    reset: function () {
+        this.currentQuestion = 0;
+        this.counter = countStartNumber;
+        this.correct = 0;
+        this.incorrect = 0;
+        this.loadQuestion();
+    }
+};
+
+// CLICK EVENTS
+
+$(document).on("click", "#start-over", function () {
+    game.reset();
 });
 
-    // COUNTDOWN TIMER
-    var timeLeft = 60;
-    var timerId;
+$(document).on("click", ".answer-button", function (e) {
+    game.clicked(e);
+});
 
-    //start and set the timer to run in 1000ms increments
-    function startTimer() {
-        timerId = setInterval(countdown, 1000);
-    }
-    function countdown() {
-        if (timeLeft !== -1) {
-            $('#time-left').html(timeLeft + ' seconds remaining');
-            timeLeft--;
-        }
-        if(timeLeft === 0){
-            $('#time-left').html('Time is up!');
-            $('#questions').hide();
-        }
-    }
-  
-    var questions = [
-        {
-            question: '1. The ocean makes up how  much of the earths surface?',
-            choices: ['40%', '71%', '36%', '87%'],
-            correctAnswer: 1
-        },
-        {
-            question: '2. Where is the deepest point?',
-            choices: ['Mariana Trench', 'Philippine Trench', 'Eurasian Basin', 'Tonga Trench'],
-            correctAnswer: 0
-        },
-        {
-            question: '3. From how far away can the Great Barrier Reef be seen?',
-            choices: ['Mesosphere', 'Top of Mount Everest', 'The Moon', 'Stratosphere'],
-            correctAnswer: 2
-        },
-        {
-            question: '4. How far does the Gray Whale migrate every year(the longest of any mammel)?',
-            choices: ['15,000 miles', '2,000 miles', '7,000 miles', '10,000 miles'],
-            correctAnswer: 3
-        },
-        {
-            question: '5. What is the largest animal on earth EVER?',
-            choices: ['Megalodon', 'Blue Whale', 'Tyrannosaurus-Rex', 'Giant Squid'],
-            correctAnswer: 1
-        },
-        {
-            question: '6. How much of the worlds volcanic activity occurs in the ocean?',
-            choices: ['20%', '40%', '70%', '90%'],
-            correctAnswer: 3
-        },
-        {
-            question: '7. How much of the underwater world remains unexplored?',
-            choices: ['95%', '73%', '16%', '0%'],
-            correctAnswer: 0
-        },
-        {
-            question: '8. Geographers divide the ocean into how many parts?',
-            choices: ['2', '3', '4', '5'],
-            correctAnswer: 2
-        },
-        {
-            question: '9. What microscopic organisms are respnosible for producing about 1/2 of the worlds oxygen?',
-            choices: ['phytoplankton', 'seagrasses', 'sea anemones', 'Acantharea'],
-            correctAnswer: 1
-        },
-        {
-            question: '10. Which shark can thrive in both salt and fresh water? ?',
-            choices: ['Sand', 'Bull', 'Thresher', 'Smooth-Hound'],
-            correctAnswer: 1
-        }
-
-        ];
-
-    function displayQuestions() {
-
-        // Loop through questions array
-        for (var i = 0; i<questions.length; i++) {
-            var question = questions[i].question;
-            var choices = questions[i].choices;
-            $('#questions').append(question + '<br>');
-
-            //My loop attempt to reduce redundancy
-        //function displayAnswers(choices) {
-          //  for (var j = 0; j < questions[index].choices.length; j++) {
-            //    var radio = $("<input>").attr({ type: 'radio', value: questions[index].choices[j], name: 'radio' + index });
-              //  $('#questions').append("<label>" + radio);
-           // }
-
-       // }
-    //}
-
-            $('#questions').append("<label>" + "<input type='radio' name='quiz" + question[0] + "'id='" + (0) + "'value='" + choices[0] + "'>" + ' ' + choices[0] + "</label><br>");
-            $('#questions').append("<label>" + "<input type='radio' name='quiz" + question[0] + "'id='" + (1) + "'value='" + choices[1] + "'>" + ' ' + choices[1] + "</label><br>");
-            $('#questions').append("<label>" + "<input type='radio' name='quiz" + question[0] + "'id='" + (2) + "'value='" + choices[2] + "'>" + ' ' + choices[2] + "</label><br>");
-            $('#questions').append("<label>" + "<input type='radio' name='quiz" + question[0] + "'id='" + (3) + "'value='" + choices[3] + "'>" + ' ' + choices[3] + "</label><br><br>");
-        
-        }
-    }
-        /*if( choice === correctAnswer){
-            correctAnswer ++;
-        }
-        else{
-            wrongAnswer++;
-        }
-    //The results section.....
-    function results(){
-        for (var i = 0; i<questions.length; i++) {
-            var radios = document.getElementsByName('quiz' + (i+1));
-        $(#correctAnswer).html()
-
-        LITERALLY TRYING ALL THE THING.... SOOO TIRED, GOING TO BED NOW!
-
-
-
-
-
-    }
-
-
-    }*/
+$(document).on("click", "#start", function () {
+    $("#sub-wrapper").prepend("<h2>Time Remaining: <span id='counter-number'>30</span> Seconds</h2>");
+    game.loadQuestion();
+});
